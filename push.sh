21633010aee8b6ca7c9003ced66ae5075440073a
Vasishath Kaushal <vasishathkaushal1@ipDialog-India-3s-Mac-mini.local>
2021-12-30 17:24:32 +0530

    Update

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
-echo $(date +%s) > last_commit.txt
-git add last_commit.txt
+echo $(date +%s) > assets/assets/last_commit.txt
+git add assets/assets/last_commit.txt
 git add NeuvenaMobile.ipa && git commit -m "Update app" && git push -f https://vasishathdev@bitbucket.org/vasishathdev/vasishathdev.bitbucket.io
